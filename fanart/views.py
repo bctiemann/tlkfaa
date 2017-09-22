a215568c691e88c8ec7545cca93cfc44b1acdd49
Brian Tiemann <btman@lionking.org>
2017-09-22 15:30:21 -0400

    Contests box context

--- fanart/views.py
+++ fanart/views.py
@@ -11,7 +11,7 @@ from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 
-from fanart.models import Contest, Favorite, TradingOffer, TradingClaim, ColoringBase
+from fanart.models import Contest, Favorite, TradingOffer, TradingClaim, ColoringBase, Contest
 
 from datetime import timedelta
 
@@ -19,60 +19,68 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class UserBoxView(TemplateView):
+class UserPaneView(TemplateView):
 
-    def get_context_data(self, **kwargs):
-        context = super(UserBoxView, self).get_context_data(**kwargs)
-        context['settings'] = settings
-        context['sketcher_users'] = range(12)
-
+    def get_community_art_data(self):
+        community_art_data = {}
         icons_publish_start_date = timezone.now() - timedelta(weeks=3)
         adoptables_publish_start_date = timezone.now() - timedelta(days=30*6)
         adoptables_mine_start_date = timezone.now() - timedelta(days=30)
-        context['icons'] = TradingOffer.objects.filter(type='icon', is_active=True, is_visible=True, date_posted__gt=icons_publish_start_date)
-        context['icons_today'] = TradingOffer.objects.filter(type='icon', is_active=True, is_visible=True, date_posted__date__gte=timezone.now())
-        context['icons_mine'] = TradingClaim.objects.filter(offer__type='icon', offer__is_active=True, offer__is_visible=True, date_fulfilled__isnull=True, filename='', offer__date_posted__gt=icons_publish_start_date, user=self.request.user)
-        context['adoptables'] = TradingOffer.objects.filter(type='adoptable', is_active=True, is_visible=True, date_posted__gt=adoptables_publish_start_date)
-        context['adoptables_unclaimed'] = context['adoptables'].filter(adopted_by__isnull=True)
-        context['adoptables_mine'] = TradingClaim.objects.filter(offer__type='adoptable', offer__is_active=True, offer__is_visible=True, date_fulfilled__isnull=False, offer__date_posted__gt=adoptables_mine_start_date, user=self.request.user)
-        context['coloring_bases'] = ColoringBase.objects.filter(is_active=True, is_visible=True)
+        community_art_data['icons'] = TradingOffer.objects.filter(type='icon', is_active=True, is_visible=True, date_posted__gt=icons_publish_start_date)
+        community_art_data['icons_today'] = TradingOffer.objects.filter(type='icon', is_active=True, is_visible=True, date_posted__date__gte=timezone.now())
+        community_art_data['icons_mine'] = TradingClaim.objects.filter(offer__type='icon', offer__is_active=True, offer__is_visible=True, date_fulfilled__isnull=True, filename='', offer__date_posted__gt=icons_publish_start_date, user=self.request.user)
+        community_art_data['adoptables'] = TradingOffer.objects.filter(type='adoptable', is_active=True, is_visible=True, date_posted__gt=adoptables_publish_start_date)
+        community_art_data['adoptables_unclaimed'] = community_art_data['adoptables'].filter(adopted_by__isnull=True)
+        community_art_data['adoptables_mine'] = TradingClaim.objects.filter(offer__type='adoptable', offer__is_active=True, offer__is_visible=True, date_fulfilled__isnull=False, offer__date_posted__gt=adoptables_mine_start_date, user=self.request.user)
+        community_art_data['coloring_bases'] = ColoringBase.objects.filter(is_active=True, is_visible=True)
+        return community_art_data
+
+    def get_contests_data(self):
+        contests_data = {}
+        contests_data['contests_new'] = Contest.objects.filter(type='personal', is_active=True, is_cancelled=False, date_end__gt=timezone.now()).order_by('-date_start')[0:5]
+        contests_data['contests_ending'] = Contest.objects.filter(type='personal', is_active=True, is_cancelled=False, date_end__gt=timezone.now()).order_by('-date_end')[0:5]
+        return contests_data
+
+    def get_context_data(self, **kwargs):
+        context = super(UserPaneView, self).get_context_data(**kwargs)
+        context['settings'] = settings
+        context['sketcher_users'] = range(12)
 
         return context
 
 
-class HomeView(UserBoxView):
+class HomeView(UserPaneView):
     template_name = 'fanart/home.html'
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['current_contest'] = Contest.objects.filter(type='global', is_active=True, date_start__lt=timezone.now()).order_by('-date_created').first()
-#        context['favorite_artists'] = Favorite.objects.for_user(self.request.user)
-        context['settings'] = settings
-
+        context['community_art_data'] = self.get_community_art_data()
+        context['contests_data'] = self.get_contests_data()
         return context
 
 
-class ArtistsView(TemplateView):
+class ArtistsView(UserPaneView):
     template_name = 'fanart/artists.html'
 
 
-class ArtworkView(TemplateView):
+class ArtworkView(UserPaneView):
     template_name = 'fanart/artwork.html'
 
 
-class CharactersView(TemplateView):
+class CharactersView(UserPaneView):
     template_name = 'fanart/characters.html'
 
 
-class TradingTreeView(TemplateView):
+class TradingTreeView(UserPaneView):
     template_name = 'fanart/tradingtree.html'
 
 
-class ColoringCaveView(TemplateView):
+class ColoringCaveView(UserPaneView):
     template_name = 'fanart/coloringcave.html'
 
 
-class SpecialFeaturesView(TemplateView):
+class SpecialFeaturesView(UserPaneView):
     template_name = 'fanart/special.html'
 
 
@@ -85,21 +93,31 @@ class UserBoxSetView(APIView):
         return Response(response)
 
 
-class FavoriteArtistsBoxView(UserBoxView):
+class FavoriteArtistsBoxView(UserPaneView):
     template_name = 'fanart/userpane/favorite_artists.html'
 
-class FavoritePicturesBoxView(UserBoxView):
+class FavoritePicturesBoxView(UserPaneView):
     template_name = 'fanart/userpane/favorite_pictures.html'
 
-class SketcherBoxView(UserBoxView):
+class SketcherBoxView(UserPaneView):
     template_name = 'fanart/userpane/sketcher.html'
 
-class CommunityArtBoxView(UserBoxView):
+class CommunityArtBoxView(UserPaneView):
     template_name = 'fanart/userpane/community_art.html'
 
-class ContestsBoxView(UserBoxView):
+    def get_context_data(self, **kwargs):
+        context = super(CommunityArtBoxView, self).get_context_data(**kwargs)
+        context['community_art_data'] = self.get_community_art_data()
+        return context
+
+class ContestsBoxView(UserPaneView):
     template_name = 'fanart/userpane/contests.html'
 
-class ToolBoxView(UserBoxView):
+    def get_context_data(self, **kwargs):
+        context = super(ContestsBoxView, self).get_context_data(**kwargs)
+        context['contests_data'] = self.get_contests_data()
+        return context
+
+class ToolBoxView(UserPaneView):
     template_name = 'fanart/userpane/toolbox.html'
 
