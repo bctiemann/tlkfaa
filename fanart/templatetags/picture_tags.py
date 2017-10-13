d8c85a3442f9f1a1a6b5cb1b439d14513d3b94ad
Brian Tiemann <btman@lionking.org>
2017-10-12 23:22:39 -0400

    Fave tag

--- fanart/templatetags/picture_tags.py
+++ fanart/templatetags/picture_tags.py
@@ -1,7 +1,7 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 
-from fanart.models import UnviewedPicture
+from fanart.models import UnviewedPicture, Favorite
 
 import logging
 logger = logging.getLogger(__name__)
@@ -27,6 +27,19 @@ class ViewPictureNode(template.Node):
         return 'newpic' if is_unviewed else ''
 
 
+class IsFavoriteNode(template.Node):
+
+    def __init__(self, picture, user):
+        self.picture = template.Variable(picture)
+        self.user = template.Variable(user)
+
+    def render(self, context):
+        picture = self.picture.resolve(context)
+        user = self.user.resolve(context)
+
+        return 'isfave' if Favorite.objects.filter(picture=picture, user=user).exists() else ''
+
+
 @register.tag(name='view_picture')
 def do_view_picture(parser, token):
     try:
@@ -37,4 +50,12 @@ def do_view_picture(parser, token):
         )
     return ViewPictureNode(picture, user)
 
-
+@register.tag(name='is_favorite')
+def get_is_favorite(parser, token):
+    try:
+        tag_name, picture, user = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError(
+            "%r tag requires two arguments, a picture and a user" % token.contents.split()[0]
+        )
+    return IsFavoriteNode(picture, user)
