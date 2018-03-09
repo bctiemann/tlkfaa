5e6153301357114ae31b05f1b5c196cbaeae1a57
Brian Tiemann <btman@lionking.org>
2018-03-08 23:01:38 -0500

    Better line parsing for mod notes import script

--- fanart/management/commands/populate_mod_notes.py
+++ fanart/management/commands/populate_mod_notes.py
@@ -17,16 +17,32 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for user in models.User.objects.exclude(comments=''):
             existing_notes = filter(lambda x: x != '', user.comments.splitlines())
-#            print user, existing_notes
-            existing_notes[0] = existing_notes[0].replace('/', '-')
-            try:
-                date_created = datetime.datetime.strptime(existing_notes[0], '%m-%d-%Y')
-            except ValueError:
-                date_created = datetime.datetime.strptime(existing_notes[0], '%m-%d-%y')
-            print user, date_created, existing_notes
-            mod_note = models.ModNote.objects.create(
-                artist = user,
-                note = existing_notes[1],
-            )
-            mod_note.date_created = date_created
-            mod_note.save()
+            print user, existing_notes
+            notes = []
+            note = None
+            for line in existing_notes:
+#                if line[0] in [unicode(x) for x in range(10)]:
+#                if len(line) < 11:
+                if re.match('^[0-9-/]+$', line):
+                    print len(line)
+                    if note:
+                        notes.append(note)
+                    line = line.replace('/', '-')
+                    note = {'date_created': line}
+                else:
+                    note['note'] = line
+            notes.append(note)
+            print notes
+            for note in notes:
+                print note
+                try:
+                    date_created = datetime.datetime.strptime(note['date_created'], '%m-%d-%Y')
+                except ValueError:
+                    date_created = datetime.datetime.strptime(note['date_created'], '%m-%d-%y')
+                print user, date_created, existing_notes
+                mod_note = models.ModNote.objects.create(
+                    artist = user,
+                    note = note['note'],
+                )
+                mod_note.date_created = date_created
+                mod_note.save()
