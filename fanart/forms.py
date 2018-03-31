048eba2eff3d6568df49a6796fe03b53736daf6d
Brian Tiemann <brian.tiemann@essencedigital.com>
2018-03-31 07:31:36 -0400

    Encode username in logging login

--- fanart/forms.py
+++ fanart/forms.py
@@ -64,7 +64,7 @@ class LoginForm(AuthenticationForm):
     def clean(self):
         m = hashlib.md5()
         username = self.cleaned_data.get('username')
-        logger.info('Login: {0}'.format(username))
+        logger.info('Login: {0}'.format(username.encode('utf8')))
         raw_password = self.cleaned_data.get('password')
 
         if username is not None and raw_password:
