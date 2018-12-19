57b280f30c3b993e0f6819b0f568680f6a1022f9
Brian Tiemann <btman@mac.com>
2018-12-19 14:19:33 -0500

    Add PM admin

--- pms/admin.py
+++ pms/admin.py
@@ -1,6 +1,16 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from fanart import models as fanart_models
+from pms import models as pms_models
+from django_extensions.admin import ForeignKeyAutocompleteAdmin
 
-# Register your models here.
+import logging
+logger = logging.getLogger(__name__)
+
+
+class PrivateMessageAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'sender', 'recipient', 'date_sent', 'message',)
+    list_filter = ()
+    readonly_fields = ('sender', 'recipient',)
+    user_id_for_formfield = None
+admin.site.register(pms_models.PrivateMessage, PrivateMessageAdmin)
+
