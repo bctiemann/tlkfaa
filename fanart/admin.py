04baa549a5838526affaedc36f1b58bd10600022
Brian Tiemann <btman@mac.com>
2019-08-31 11:37:20 -0400

    Don't make user read-only

--- fanart/admin.py
+++ fanart/admin.py
@@ -213,7 +213,6 @@ admin.site.register(fanart_models.ContestEntry, ContestEntryAdmin)
 class BulletinAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'date_posted', 'is_published', 'is_admin', 'num_replies',)
     list_filter = ('is_admin',)
-    readonly_fields = ('user',)
     autocomplete_fields = ('user',)
     user_id_for_formfield = None
 admin.site.register(fanart_models.Bulletin, BulletinAdmin)
