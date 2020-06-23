1ca63f6706c85af3c113bae34b32e0df766d3488
Brian Tiemann <btman@mac.com>
2020-06-22 21:40:21 -0400

    Put thumb_status pending loop in a try-except

--- fanart/views/approval.py
+++ fanart/views/approval.py
@@ -404,12 +404,15 @@ class PendingThumbStatusView(APIView):
 
     def get(self, request, pending_id=None):
         response = {}
-        for pending in models.Pending.objects.all():
-            response[pending.id] = {
-                'thumbnail_url': pending.thumbnail_url,
-                'preview_url': pending.preview_url,
-                'thumbnail_done': pending.thumbnail_created,
-                'preview_done': pending.preview_created,
-            }
+        try:
+            for pending in models.Pending.objects.all():
+                response[pending.id] = {
+                    'thumbnail_url': pending.thumbnail_url,
+                    'preview_url': pending.preview_url,
+                    'thumbnail_done': pending.thumbnail_created,
+                    'preview_done': pending.preview_created,
+                }
+        except FileNotFoundError:
+            pass
         return Response(response)
 
