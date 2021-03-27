75db5af76ac8b79c8aad1a4a7d702b82476eefe7
Brian Tiemann <btman@mac.com>
2021-03-26 21:12:54 -0400

    Remove unused imports from comments views

--- artmanager/views/comments.py
+++ artmanager/views/comments.py
@@ -1,41 +1,17 @@
-
-
 from django.conf import settings
-from django.shortcuts import render, render_to_response, redirect, reverse, get_object_or_404
-from django.http import HttpResponse, JsonResponse, Http404, HttpResponseForbidden, HttpResponseRedirect
-from django.core.exceptions import PermissionDenied
-from django.views.generic import TemplateView
-from django.views.generic.base import RedirectView
+from django.shortcuts import reverse, get_object_or_404
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView, FormMixin
-from django.utils import timezone
-from django.shortcuts import render
-from django.contrib.auth import update_session_auth_hash
-from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.db.models import Q, OuterRef, Subquery, Min, Max, Count
-
-from django.core.exceptions import ValidationError
-#from django.utils.translation import ugettext_lazy as _
+from django.core.paginator import Paginator, EmptyPage
+from django.db.models import Q
 
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 
-from fanart import models, utils, tasks
-from fanart.views import UserPaneMixin
-from fanart.forms import AjaxableResponseMixin
+from fanart import models, utils
 from artmanager import forms
-from trading_tree.models import Offer, Claim
-from coloring_cave.models import Base, ColoringPicture
 from . import ArtManagerPaneView
-
-import json
-import hashlib
-import os
-import shutil
-from PIL import Image
 
 import logging
 logger = logging.getLogger(__name__)
