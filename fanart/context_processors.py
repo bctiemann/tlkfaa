f00affb76357aef2c607400a46fc1a4529bbc7d7
Brian Tiemann <btman@mac.com>
2024-06-16 22:40:19 -0400

    30th Anniversary commemorative banner image (animated PNG of dust swirl forming "30")

--- fanart/context_processors.py
+++ fanart/context_processors.py
@@ -5,5 +5,6 @@ def settings_constants(request):
     context = {
         'admin_name': settings.ADMIN_NAME,
         'admin_email': settings.ADMIN_EMAIL,
+        '30th_anniversary': settings.ANNIVERSARY_30,
     }
     return context
